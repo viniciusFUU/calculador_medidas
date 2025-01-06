4b9a04efb2ba428e2da41d6e56c4e8f993449064
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2025-01-06 10:08:30 -0300

    criação entidade service

---
+++ project/src/main/java/project/valuesCalculator/Models/Service.java
@@ -0,0 +1,49 @@
+package project.valuesCalculator.Models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "service")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Service {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_service")
+    private int idService;
+
+    @ManyToOne
+    @JoinColumn(name = "id_client")
+    private int idClient;
+
+    @Column(name = "service_description")
+    private String serviceDescription;
+
+    @Column(name = "service_measure_side")
+    private double serviceMeasureSide;
+
+    @Column(name = "service_measure_heigth")
+    private double serviceMeasureHeigth;
+
+    @Column(name = "service_measure_base")
+    private double serviceMeasureBase;
+
+    @Column(name = "meter_value")
+    private double meterValue;
+
+    @Column(name = "service_value")
+    private double serviceValue;
+}
